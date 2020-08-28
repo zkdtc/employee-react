63aa07c579a219c8d7fcd13c8adb8ef5cf728ca0
zhangkai <zkdtckk@gmail.com>
2020-08-28 16:24:15 -0700

    no warning now

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,30 +1,25 @@
-import React, { useContext }  from "react";
+import React, { useContext } from "react";
 import "./style.css";
 import UserContext from "../../utils/userContext";
 
 function Cards(props) {
-    const { employeesState } = useContext(UserContext);
-    console.log(employeesState);
-    const tempArr0=employeesState.sort?employeesState.employees.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)):employeesState.employees; 
-    const tempArr=employeesState.filter? tempArr0.filter(item=>item.title===employeesState.selected):employeesState.employees;
-    
+  const { employeesState } = useContext(UserContext);
+  const tempArr0 = employeesState.sort ? employeesState.employees.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)) : employeesState.employees;
+  const tempArr = employeesState.filter ? tempArr0.filter(item => item.title === employeesState.selected) : employeesState.employees;
+
 
   return (
     <div>
-    {tempArr.map(item=>(
-    <div
-    className="card h-50"
-    style={{
-      backgroundImage: props.image ? `url(${props.image})` : "none"
-    }}>
-    <div className="card-body">
-<h5 className="card-title">{item.name}</h5>
-    <p className="card-text">{item.title}</p>
+      {tempArr.map(item => (
+        <div className="card h-50" key={item.name}>
+          <div className="card-body">
+            <h5 className="card-title">{item.name}</h5>
+            <p className="card-text">{item.title}</p>
 
-  </div>
-  </div>
-    ))}
-  </div>
+          </div>
+        </div>
+      ))}
+    </div>
 
   );
 }
