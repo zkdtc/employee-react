f8427b585c8a929b11d27875fa9a1733c3912017
zhangkai <zkdtckk@gmail.com>
2020-09-09 21:32:56 -0700

    Updates

---
+++ precache-manifest.4037272c6736155a4701236033d8032d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c965bdcfa2d1be9da7c0105bdb6832cd",
+    "url": "/employee-react/index.html"
+  },
+  {
+    "revision": "af4d97253aaa6f438d6b",
+    "url": "/employee-react/static/css/main.3cc3a58c.chunk.css"
+  },
+  {
+    "revision": "d4959a7096b59f2482f2",
+    "url": "/employee-react/static/js/2.b58d0620.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/employee-react/static/js/2.b58d0620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4d97253aaa6f438d6b",
+    "url": "/employee-react/static/js/main.0c44954f.chunk.js"
+  },
+  {
+    "revision": "52c42f5cdf5c62b14c9d",
+    "url": "/employee-react/static/js/runtime-main.64b5765b.js"
+  }
+]);
